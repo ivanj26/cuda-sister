d12f61770ac004d7ae3f05f32d8ea97f5c64cfaa
ivanj09 <xmillenium.ij@gmail.com>
2019-04-11 15:25:46 +0700

    Learn cuda

--- src/main.cu
+++ src/main.cu
@@ -1,35 +1,56 @@
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <stdlib.h>
 #include <stdio.h>
- 
-const int N = 16; 
-const int blocksize = 16; 
- 
-__global__ void hello(char *a, int *b) {
-	a[threadIdx.x] += b[threadIdx.x];
+
+__global__ void vector_add(int *out, int *a, int *b, int n){
+    int index = threadIdx.x;
+    int stride = blockDim.x;
+
+    for (int i = index; i < n; i+=stride) {
+        out[i] = a[i] + b[i];
+    }
 }
- 
+
 int main() {
-	char a[N] = "Hello \0\0\0\0\0\0";
-	int b[N] = {15, 10, 6, 0, -11, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
- 
-	char *ad;
-	int *bd;
-	const int csize = N*sizeof(char);
-	const int isize = N*sizeof(int);
- 
-	printf("%s", a);
- 
-	cudaMalloc( (void**)&ad, csize ); 
-	cudaMalloc( (void**)&bd, isize ); 
-	cudaMemcpy( ad, a, csize, cudaMemcpyHostToDevice ); 
-	cudaMemcpy( bd, b, isize, cudaMemcpyHostToDevice ); 
-	
-	dim3 dimBlock( blocksize, 1 );
-	dim3 dimGrid( 1, 1 );
-	hello<<<dimGrid, dimBlock>>>(ad, bd);
-	cudaMemcpy( a, ad, csize, cudaMemcpyDeviceToHost ); 
-	cudaFree( ad );
-	cudaFree( bd );
-	
-	printf("%s\n", a);
-	return EXIT_SUCCESS;
+    int n = 10;
+    int *a, *b, *out, *dev_a, *dev_b, *dev_out;
+
+    a = (int*) malloc(n * sizeof(int));
+    b = (int*) malloc(n * sizeof(int));
+    out = (int*) malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        a[i] = 1;
+        b[i] = 2;
+    }
+
+    // Allocate device memory
+    cudaMalloc((void**)&dev_a, sizeof(float) * n);
+    cudaMalloc((void**)&dev_b, sizeof(float) * n);
+    cudaMalloc((void**)&dev_out, sizeof(float) * n);
+
+    // Transfer data from host to device memory
+    cudaMemcpy(dev_a, a, sizeof(float) * n, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_b, b, sizeof(float) * n, cudaMemcpyHostToDevice);
+
+    // Executing kernel
+    vector_add<<<1,256>>>(dev_out, dev_a, dev_b, n);
+
+    // Transfer data back to host memory
+    cudaMemcpy(out, dev_out, sizeof(float) * n, cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", out[i]);
+    }
+
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+    cudaFree(dev_out);
+
+    delete[] a;
+    delete[] out;
+    delete[] b;
+
+    return EXIT_SUCCESS;
 }
